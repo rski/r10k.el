cedb424608ad70ef576aa9a1ef0610de0c0392dd
Romanos Skiadas <rski@intracom-telecom.com>
2016-08-02 10:34:26 +0300

    Add boilerplate

---
+++ README.md
@@ -0,0 +1,13 @@
+# r10k.el
+
+## Synopsis
+
+Run r10k commands from within emacs.
+
+These commands can optionally use the Puppetfile from the current project.
+
+## Installation
+
+## Usage
+
+## See also
---
+++ r10k.el
@@ -0,0 +1,7 @@
+;;; r10k.el --- Run r10k commands from inside emacs
+
+;;; Version: 0.0.1
+;;; Author: Romanos Skiadas <rom.skiad@gmail.com>
+;;; Url: https://github.com/rski/r10k.el
+;;; Keywords: r10k puppet
+;;; Created: 02 August 2016
